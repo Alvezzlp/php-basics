a2f24751eb4c416a9529bde88b6a904c29420e83
Victor Alvez <victor.a.silva34@aluno.senai.br>
2025-10-10 16:32:26 -0300

    Introdução

---
+++ Aula1/exemplo3.php
@@ -0,0 +1,13 @@
+<?php 
+    echo                              "testando espaço em branco <br>";
+    echo "testando espaço em branco";
+
+    // O caracter &nbsp; adiciona um espaço em branco dentro da string
+    echo "&#160; &nbsp;                                 testando          espaço          em          branco <br>";
+    echo
+    "quebra de linha <br>"
+    ;
+
+    echo "quebra de linha <br>"
+    ;
+?>
