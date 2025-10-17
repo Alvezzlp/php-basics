1c14f0aed7ef54142eaf6fa9d0abc458a257e5a6
Victor Alvez <victor.a.silva34@aluno.senai.br>
2025-10-17 10:43:31 -0300

    Ajustar arquivos no repositório
    Mover os arquivos para a pasta raiz do repositório

--- php-basicos(alunos)/2_opera_variaveis.php
+++ php-basicos(alunos)/2_opera_variaveis.php
@@ -2,7 +2,7 @@
 
 // Montagem da URL
 
-// http://localhost/php-basics/php-basicos(alunos)/2_opera_variaveis.php?numero1=10&numero2=5
+// http://localhost/php-basics/php-basicos(alunos)/2_opera_variaveis/.php?numero1=10&numero2=5
 
 // Variáveis que recebem valores pela URL (Método GET)
 $numero1 = $_GET['numero1'];
